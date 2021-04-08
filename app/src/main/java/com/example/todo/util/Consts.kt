a9f22219e85ddaefa4db1f3bfa7d34c184d64626
berkanturkali <brktrkali@outlook.com>
2021-04-08 04:58:41 +0300

    todo calendar

--- app/src/main/java/com/example/todo/util/Consts.kt
+++ app/src/main/java/com/example/todo/util/Consts.kt
@@ -1,17 +1,29 @@
 package com.example.todo.util
 
 import com.example.todo.BuildConfig
+import com.example.todo.R
 
 class Consts {
 
-    companion object{
-        const val BASE_URL =BuildConfig.BASE_URL
-        const val TIME_OUT=3000L
+    companion object {
+        const val BASE_URL = BuildConfig.BASE_URL
+        const val TIME_OUT = 3000L
         const val CONNECTION_TIMEOUT: Long = 30L
         const val READ_TIMEOUT: Long = 30L
         const val WRITE_TIMEOUT: Long = 30L
         const val FILE_NAME = "photo"
         const val PREF_NAME = "token"
-        const val USER_ID ="userId"
+        const val USER_ID = "userId"
+
+        val CATEGORIES = mapOf(
+            "Work" to R.drawable.ic_category_work,
+            "Music" to R.drawable.ic_category_music,
+            "Travel" to R.drawable.ic_category_travel,
+            "Study" to R.drawable.ic_category_study,
+            "Home" to R.drawable.ic_category_home,
+            "Shopping" to R.drawable.ic_category_shopping
+        )
+
+
     }
 }
--- app/src/main/java/com/example/todo/view/fragments/homeflow/AddTodoFragment.kt
+++ app/src/main/java/com/example/todo/view/fragments/homeflow/AddTodoFragment.kt
@@ -2,14 +2,51 @@ package com.example.todo.view.fragments.homeflow
 
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
+import com.example.todo.R
 import com.example.todo.databinding.FragmentAddTodoLayoutBinding
+import com.example.todo.util.Consts
 import com.example.todo.view.fragments.BaseFragment
+import com.google.android.material.datepicker.MaterialDatePicker
 import dagger.hilt.android.AndroidEntryPoint
+import java.text.SimpleDateFormat
+import java.util.*
+
+private const val TAG = "AddTodoFragment"
 
 @AndroidEntryPoint
-class AddTodoFragment:BaseFragment<FragmentAddTodoLayoutBinding>(FragmentAddTodoLayoutBinding::inflate) {
+class AddTodoFragment :
+    BaseFragment<FragmentAddTodoLayoutBinding>(FragmentAddTodoLayoutBinding::inflate) {
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initWidgets()
+    }
 
+    private fun initWidgets() {
+        val list = mutableListOf<String>()
+        for (key in Consts.CATEGORIES.keys) {
+            list.add(key)
+        }
+        val adapter = ArrayAdapter(
+            requireContext(),
+            R.layout.category_list_item_layout,
+            R.id.category,
+            list
+        )
+        binding.categoryEt.setAdapter(adapter)
+        binding.datePickerIb.setOnClickListener {
+            val datePicker = MaterialDatePicker.Builder.datePicker()
+                .setTitleText("Select dates")
+                .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+                .build()
+            datePicker.show(requireActivity().supportFragmentManager, "tag")
+            datePicker.addOnPositiveButtonClickListener {
+                val calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"))
+                calendar.timeInMillis = it
+                val dateFormat = SimpleDateFormat("dd/MM/yyyy")
+                binding.dateEt.setText(dateFormat.format(calendar.timeInMillis))
+            }
+        }
     }
 }
